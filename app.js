ed781cf951be344d37a1d7fb0ac3af6917157880
dilipMaharjan <dilipmaharjann@gmail.com>
2017-12-05 16:36:44 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// const HTTP=require('http');
+
+
+// const HOSTNAME='127.0.0.1';
+// const PORT='3000';
+
+// const server=HTTP.createServer((req,res)=>{
+//     res.statusCode=200;
+//     res.setHeader('Content-Type','text/plain');
+//     res.end('Hello Node');
+// });
+// server.listen(PORT,HOSTNAME,()=>{
+//     console.log(`Server running at http:// ${HOSTNAME}:${PORT}`)
+// });
