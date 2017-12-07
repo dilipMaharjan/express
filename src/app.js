ab590e90b28e961f1411f64bfac59fbd9fd0a4dd
crazdrms <dilipmaharjann@gmail.com>
2017-12-06 22:31:19 -0500

    fake data

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-const express =require('express');
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
 
 const app=express();
+
 app.disable('x-powered-by');
 
-app.get('/',(req,res)=>{
-res.send("Hello Express!");
-});
-const PORT='3000';
+app.use('/graphql', expressGraphQL({
+    schema:schema,
+    graphiql:true
+}));
+
+const PORT='4000';
 app.listen(PORT,()=>{
     console.log(`Server is running at port:${PORT}`)
 });
